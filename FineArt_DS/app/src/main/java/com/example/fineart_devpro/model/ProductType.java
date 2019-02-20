faaec9e339e02395ff6440722f1b8a3172fbbaa7
DuongVanSon <32009803+DS0912@users.noreply.github.com>
2019-02-20 14:15:48 +0700

    GUI
    commit by DS

---
+++ FineArt_DS/app/src/main/java/com/example/fineart_devpro/model/ProductType.java
@@ -0,0 +1,39 @@
+package com.example.fineart_devpro.model;
+
+public class ProductType {
+    public int id;
+    public String productTypeName;
+    public String productTypeImage;
+
+    public ProductType(int id, String productTypeName, String productTypeImage) {
+        this.id = id;
+        this.productTypeName = productTypeName;
+        this.productTypeImage = productTypeImage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductTypeName() {
+        return productTypeName;
+    }
+
+    public void setProductTypeName(String productTypeNam) {
+        this.productTypeName = productTypeName;
+    }
+
+    public String getProductTypeImage() {
+        return productTypeImage;
+    }
+
+    public void setProductTypeImage(String productTypeImage) {
+        this.productTypeImage = productTypeImage;
+    }
+
+
+}
---
+++ FineArt_DS/app/src/main/java/com/example/fineart_devpro/util/Server.java
@@ -0,0 +1,7 @@
+package com.example.fineart_devpro.util;
+
+public class Server {
+    public static String host = "gomynghevn.com";
+    public static String urlProductType = "http://" + host + "/getproducttype.php";
+    public static String urlNewProduct = "http://" + host + "/getnewproduct.php";
+}
